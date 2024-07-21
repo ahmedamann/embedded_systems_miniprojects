339e3bec6cfe3d334274560b35412031b62b0d29
ahmedamann <149511766+ahmedamann@users.noreply.github.com>
2024-07-21 16:45:14 +0300

    UPDATE

--- Mini_Project_4_dir/miniproject4_eclipseWS/Debug/MCAL/icu.d
+++
@@ -1,8 +0,0 @@
-MCAL/icu.o: ../MCAL/icu.c ../MCAL/icu.h ../MCAL/../LIB/std_types.h \
- ../MCAL/../LIB/common_macros.h
-
-../MCAL/icu.h:
-
-../MCAL/../LIB/std_types.h:
-
-../MCAL/../LIB/common_macros.h:
